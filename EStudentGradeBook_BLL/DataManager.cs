ef84cbd2a1af2c10721daadb79fe00a7498d5fd4
sator <sator@SATOR-PC>
2016-11-25 02:43:01 +0200

    Data manager + Data mapper

---
+++ EStudentGradeBook_BLL/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudentGradeBook_BLL
+{
+    abstract class DataManager
+    {
+        public abstract void Add(object obj);
+
+        public abstract object DataMapper(object obj);
+    }
+}
---
+++ EStudentGradeBook_BLL/StudentDataManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EStudentGradeBook_BLL.DTO;
+using EStudentGradeBook_DAL;
+using AutoMapper;
+
+namespace EStudentGradeBook_BLL
+{
+    class StudentDataManager : DataManager
+    {
+        public override void Add(object obj)
+        {
+            Student student = DataMapper(obj as StudentDTO) as Student;
+            EStudentGradeBookDBContext context = new EStudentGradeBookDBContext();
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+        public override object DataMapper(object obj)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<StudentDTO, Student>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper.Map<StudentDTO, Student>(obj as StudentDTO);
+        }
+    }
+}
