019a8e7356b2f607b330332dfb0bc1c8cb94b068
sator <sator@SATOR-PC>
2016-11-29 22:46:08 +0200

    DataManager and StudentDataManager

---
+++ EStudentGradeBook_DAL/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudentGradeBook_DAL
+{
+    abstract class DataManager<T>
+    {
+        public abstract void Add(T addObj);
+        public abstract void Remove(T removeObj);
+        public abstract void Update(T updateObj);
+        public abstract List<T> GetList();
+
+    }
+}
---
+++ EStudentGradeBook_DAL/StudentDataManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStudentGradeBook_DAL
+{
+    class StudentDataManager : DataManager<Student>
+    {
+        public override void Add(Student addObj)
+        {
+            var context = new EStudentGradeBookDBContext();
+            context.Students.Add(addObj);
+            context.SaveChanges();
+        }
+
+        public override void Remove(Student removeObj)
+        {
+            var context = new EStudentGradeBookDBContext();
+            context.Students.Remove(removeObj);
+            context.SaveChanges();
+        }
+
+        public override void Update(Student updateObj)
+        {
+            var context = new EStudentGradeBookDBContext();
+            var student = context.Students.FirstOrDefault(s => s.student_id == updateObj.student_id);
+            student = updateObj;
+            context.SaveChanges();
+
+        }
+
+        public override List<Student> GetList()
+        {
+            var context = new EStudentGradeBookDBContext();
+            return context.Students.ToList();
+        }
+    }
+}
