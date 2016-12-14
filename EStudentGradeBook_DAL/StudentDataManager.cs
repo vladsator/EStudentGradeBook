9a662a425cfe8537822116b53a43c4b149aa6254
sator <sator@SATOR-PC>
2016-12-14 14:14:57 +0200

    refactor stodent data manager

--- EStudentGradeBook_DAL/StudentDataManager.cs
+++ EStudentGradeBook_DAL/StudentDataManager.cs
@@ -9,33 +9,33 @@ namespace EStudentGradeBook_DAL
 {
     class StudentDataManager : DataManager<Student>
     {
+        private readonly EStudentGradeBookDBContext _context = new EStudentGradeBookDBContext();
+
         public override void Add(Student addObj)
         {
-            var context = new EStudentGradeBookDBContext();
-            context.Students.Add(addObj);
-            context.SaveChanges();
+            _context.Students.Add(addObj);
+            _context.SaveChanges();
         }
 
         public override void Remove(Student removeObj)
         {
-            var context = new EStudentGradeBookDBContext();
-            context.Students.Remove(removeObj);
-            context.SaveChanges();
+            _context.Students.Remove(removeObj);
+            _context.SaveChanges();
         }
-
+        
+        //TODO: test this shit!
         public override void Update(Student updateObj)
         {
-            var context = new EStudentGradeBookDBContext();
-            var student = context.Students.FirstOrDefault(s => s.student_id == updateObj.student_id);
+            var student = _context.Students.FirstOrDefault(s => s.student_id == updateObj.student_id);
             student = updateObj;
-            context.SaveChanges();
+            _context.SaveChanges();
+            throw new NotImplementedException();
 
         }
 
         public override List<Student> GetList()
         {
-            var context = new EStudentGradeBookDBContext();
-            return context.Students.ToList();
+            return _context.Students.ToList();
         }
     }
 }
